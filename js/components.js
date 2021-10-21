8450e066a4c59c1781f6e180a6f59947583884dd
Justin Armstrong <jxa1762@rit.edu>
2021-10-21 17:44:47 -0400

    Nav bar updated + fixed, individual project pages with mroe details, resume page added

---
+++ js/components.js
@@ -0,0 +1,58 @@
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = 
+          "<header>\
+            <nav class='nav'>\
+              <ul class='nav__list'>\
+                <li class='nav__item'><a class='nav__link' href='resume.html'>Resume</a></li>\
+                <li class='nav__item'><a class='nav__link' href='index.html#my-work'>Work</a></li>\
+                <li class='nav__item'><a class='nav__link' href='index.html#about-me'>About Me</a></li>\
+                <li class='nav__item'><a class='nav__link' href='index.html'>Home</a></li>\
+              </ul>\
+            </nav>\
+          </header>";
+    }
+}
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = 
+    "<footer class='footer'>\
+    <p class='mail'><a class='footer__link' href='mailto:justin.strongarms10@gmail.com?subject=Portfolio Inquiry'>Email me!</a></p>\
+    <ul class='social-list'>\
+      <li class='social-list__item'>\
+        <a class='social-list__link'\
+        href='https://github.com/pigghead'\
+        target='_blank' rel='noopener noreferrer'>\
+          <i class='fab fa-github-square'></i>\
+        </a>\
+      </li>\
+      <li class='social-list__item'>\
+        <a class='social-list__link'\
+        href='https://www.linkedin.com/in/justin-armstrong-916134127/'\
+        target='_blank' rel='noopener noreferrer'>\
+          <i class='fab fa-linkedin'></i>\
+        </a>\
+      </li>\
+      <li class='social-list__item'>\
+        <a class='social-list__link'\
+        href='https://armstrongj.itch.io/'\
+        target='_blank' rel='noopener noreferrer'>\
+          <i class='fab fa-itch-io'></i>\
+        </a>\
+      </li>\
+    </ul>\
+  </footer>";
+  }
+}
+
+customElements.define('header-component', Header);
+customElements.define('contact-footer', Footer);
